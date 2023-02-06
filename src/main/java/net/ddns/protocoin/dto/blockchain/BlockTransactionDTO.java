4003e225a36587d24da572ef3f7dcdca9de0550d
ngondek <natalia.gondek6@gmail.com>
2023-02-06 23:25:59 +0100

    fix: add transaction id to TransactionDTO

--- src/main/java/net/ddns/protocoin/dto/blockchain/BlockTransactionDTO.java
+++ src/main/java/net/ddns/protocoin/dto/blockchain/BlockTransactionDTO.java
@@ -1,6 +1,7 @@
 package net.ddns.protocoin.dto.blockchain;
 
 import net.ddns.protocoin.core.blockchain.transaction.Transaction;
+import net.ddns.protocoin.core.util.Converter;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,7 +10,10 @@ public class BlockTransactionDTO {
     private final List<TransactionInputDTO> transactionInputs;
     private final List<TransactionOutputDTO> transactionOutputs;
 
+    private final String txId;
+
     public BlockTransactionDTO(Transaction transaction) {
+        txId = Converter.byteArrayToHexString(transaction.getTxId());
         transactionInputs = transaction.getTransactionInputs().stream().map(TransactionInputDTO::new).collect(Collectors.toList());
         transactionOutputs = transaction.getTransactionOutputs().stream().map(TransactionOutputDTO::new).collect(Collectors.toList());
     }
@@ -21,4 +25,6 @@ public class BlockTransactionDTO {
     public List<TransactionOutputDTO> getTransactionOutputs() {
         return transactionOutputs;
     }
+
+    public String getTxId() {return txId; }
 }
